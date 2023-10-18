096818458c9101e48685755258c0b0e244340d93
HqRhn <141786593+HqRhn@users.noreply.github.com>
2023-10-19 01:45:57 +0200

    InitialCommit

---
+++ OrderManagement.Application/Behaviours/Validators/QueryValidators/GetOrderByReferenceValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using OrderManagement.Application.Queries;
+
+namespace OrderManagement.Application.Behaviours.Validators.QueryValidators
+{
+    public sealed class GetOrderByReferenceValidator : AbstractValidator<GetOrderByReferenceQuery>
+    {
+        public GetOrderByReferenceValidator()
+        {
+            RuleFor(x => x.OrderReference).NotEmpty().MinimumLength(3).MaximumLength(15);
+
+        }
+    }
+}
